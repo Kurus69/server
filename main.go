aea55d4e60de6230426ab7b79490ca844d8cd996
kurus69 <banyuwangi1999@gmail.com>
2022-12-14 20:08:12 +0700

    Server

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"landtick/database"
+	"landtick/pkg/connection"
+	"landtick/routes"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	connection.Database()
+	database.RunMigration()
+
+	r := mux.NewRouter()
+
+	routes.RouteInit(r.PathPrefix("/landtick").Subrouter())
+	r.PathPrefix("/uploads").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
+
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		panic("Failed to load env file")
+	}
+
+	var AllowedHeaders = handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	var AllowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "PATCH", "DELETE"})
+	var AllowedOrigins = handlers.AllowedOrigins([]string{"*"})
+
+	var port = "5000"
+	fmt.Println("server running localhost:" + port)
+
+	http.ListenAndServe("localhost:"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
+}
