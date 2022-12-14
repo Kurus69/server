a4c17999d156f91e02f85cd796e0db597fbb681e
kurus69 <banyuwangi1999@gmail.com>
2022-12-14 21:20:38 +0700

    raillway

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"landtick/pkg/connection"
 	"landtick/routes"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -30,8 +31,8 @@ func main() {
 	var AllowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "PATCH", "DELETE"})
 	var AllowedOrigins = handlers.AllowedOrigins([]string{"*"})
 
-	var port = "5000"
+	var port = os.Getenv("DB_PORT")
 	fmt.Println("server running localhost:" + port)
 
-	http.ListenAndServe("localhost:"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
+	http.ListenAndServe(":"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
 }
--- pkg/connection/mysql.go
+++ pkg/connection/mysql.go
@@ -3,7 +3,9 @@ package connection
 import (
 	"fmt"
 
-	"gorm.io/driver/mysql"
+	// "gorm.io/driver/mysql"
+
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -12,8 +14,22 @@ var DB *gorm.DB
 func Database() {
 	var err error
 
-	connection := "root:@tcp(127.0.0.1:3306)/landtick?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err = gorm.Open(mysql.Open(connection), &gorm.Config{})
+	// connection := "root:@tcp(127.0.0.1:3306)/landtick?charset=utf8mb4&parseTime=True&loc=Local"
+	// DB, err = gorm.Open(mysql.Open(connection), &gorm.Config{})
+	var DB_HOST = "containers-us-west-159.railway.app"
+	var DB_USER = "postgres"
+	var DB_PASSWORD = "SIKif6a2L2RKw4UWWPHb"
+	var DB_NAME = "railway"
+	var DB_PORT = "7643"
+
+	fmt.Println(DB_HOST)
+	fmt.Println(DB_USER)
+	fmt.Println(DB_PASSWORD)
+	fmt.Println(DB_PORT)
+	fmt.Println(DB_NAME)
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
